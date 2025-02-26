7c4255e5ac633302c060f08b106936fcbadcf98d
unknown <alnahianark@gmail.com>
2025-02-27 03:51:14 +0600

    first milestone

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Link from 'next/link';
 
 function page() {
   return (
     <div>
       <h1>This is homepage</h1>
+      <Link href='/sign-in'>Sign In</Link>
     </div>
   )
 }
